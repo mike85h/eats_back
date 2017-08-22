cb17548c8d71bb3096ec0afca3f7ed9153007037
Michael Harrington <mike85h@gmail.com>
2017-08-22 14:21:23 -0600

    fix migrations

--- migrations/20170822120858_book_author.js
+++ migrations/20170822120858_book_author.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('book_author', (table)=>{
     table.increments();
-    table.integer('book_id').references('id').inTable('book').notNull().onDelete('CASCADE');
-    table.integer('author_id').references('id').inTable('author').notNull().onDelete('CASCADE');
+    table.integer('book_id').references('book.id').onDelete('cascade');
+    table.integer('author_id').references('author.id').onDelete('cascade');
     })
 };
 
